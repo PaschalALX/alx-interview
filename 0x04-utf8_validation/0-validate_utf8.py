a47077d756c4eb70f69c0370c5eec7d1cbde719e
PaschalALX <pasmac2018@gmail.com>
2023-07-27 22:32:20 +0100

    Added Validate UTF project * 4

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -55,12 +55,6 @@ def isUTF8(num):
 def validUTF8(data):
     """ determines if a given data set represents a valid UTF-8 encoding """
 
-    if not data or type(data) != list:
-        return False
-
-    if not len(data):
-        return True
-
     for x in data:
         if not isUTF8(x):
             return False
