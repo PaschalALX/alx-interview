14abe913c4a3b22301086181b08349e758e884c6
PaschalALX <pasmac2018@gmail.com>
2023-07-27 22:55:53 +0100

    Refactored

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -26,37 +26,11 @@ def isASCII(num):
     return False
 
 
-def isUTF8(num):
-    """ checks if number is UTF8 encoded """
-
-    if isASCII(num):
-        return True
-
-    bits = reverse_bits(num)
-    header_bit_cnt = 0
-
-    while header_bit_cnt < (len(bits) - 1):
-        if not bits[header_bit_cnt]:
-            break
-        header_bit_cnt += 1
-
-    if len(bits) != header_bit_cnt * 8:
-        return False
-
-    contag_bit_idx = 8
-    while contag_bit_idx < len(bits):
-        if not bits[contag_bit_idx] == 1 and bits[contag_bit_idx + 1] == 0:
-            return False
-        contag_bit_idx += 8
-
-    return True
-
-
 def validUTF8(data):
     """ determines if a given data set represents a valid UTF-8 encoding """
 
     for x in data:
-        if not isUTF8(x):
+        if not isASCII(x):
             return False
 
     return True
