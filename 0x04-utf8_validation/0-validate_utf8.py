2a8f1a833a3734661debb402ee2318027e80c16e
PaschalALX <pasmac2018@gmail.com>
2023-08-04 02:49:16 +0100

    Added

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,36 +1,3 @@
 #!/usr/bin/python3
 
-""" Determines if a given data set represents a valid UTF-8 encoding """
-
-
-def reverse_bits(num):
-    """ reverses bits and returns list of bits """
-
-    bits = []
-
-    while num:
-        bit = num & 1
-        bits.append(bit)
-        num >>= 1
-
-    bits.reverse()
-    return bits
-
-
-def isASCII(num):
-    """ checks if number is ASCII encoded """
-
-    if num < 128:
-        return True
-
-    return False
-
-
-def validUTF8(data):
-    """ determines if a given data set represents a valid UTF-8 encoding """
-
-    for x in data:
-        if not isASCII(x):
-            return False
-
-    return True
+print(format(766783, '08b')[-8:])
---
+++ 0x04-utf8_validation/0-validate_utf8__.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+""" Determines if a given data set represents a valid UTF-8 encoding """
+
+
+def reverse_bits(num):
+    """ reverses bits and returns list of bits """
+
+    bits = []
+
+    while num:
+        bit = num & 1
+        bits.append(bit)
+        num >>= 1
+
+    bits.reverse()
+    return bits
+
+
+def isASCII(num):
+    """ checks if number is ASCII encoded """
+
+    if num < 128:
+        return True
+
+    return False
+
+
+def isUTF8(num):
+    """ checks if number is UTF8 encoded """
+
+    if isASCII(num):
+        return True
+
+    bits = reverse_bits(num)
+    header_bit_cnt = 0
+
+    while header_bit_cnt < (len(bits) - 1):
+        if not bits[header_bit_cnt]:
+            break
+        header_bit_cnt += 1
+
+    if len(bits) != header_bit_cnt * 8:
+        return False
+
+    contag_bit_idx = 8
+    while contag_bit_idx < len(bits):
+        if not (bits[contag_bit_idx] == 1 and bits[contag_bit_idx + 1] == 0):
+            return False
+        contag_bit_idx += 8
+
+    return True
+
+
+def validUTF8(data):
+    """ determines if a given data set represents a valid UTF-8 encoding """
+
+    for x in data:
+        if not isUTF8(x):
+            return False
+
+    return True
